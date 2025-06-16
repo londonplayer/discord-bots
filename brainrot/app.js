e032954178129b3fe342c6817ec924e5eca049ee
londonplayer <jvgerhardt@gmail.com>
2025-06-16 09:10:48 -0300

    n sei

--- brainrot/app.js
+++ brainrot/app.js
@@ -1,70 +1,79 @@
-import { Client, GatewayIntentBits, EmbedBuilder, Collection } from "discord.js";
-import dotenv from "dotenv";
-import fs from "node:fs";
-import path from "node:path";
+const { Client, GatewayIntentBits, EmbedBuilder, Collection } =
+  require("discord.js");
+const dotenv = require("dotenv");
+const fs = require("node:fs");
+const path = require("node:path");
 
 dotenv.config();
 
 const content = [
-	{ name: "bombardiro", imageLink: "./images/bombardiro_crocodilo.png" },
-	{ name: "tralalero", imageLink: "./images/tralaleiro.jpeg" },
-	{ name: "lirili", imageLink: "./images/lirili_larila.jpeg" },
-	{ name: "sahur", imageLink: "./images/tung_tunh.jpeg" },
-	{ name: "cavallo", imageLink: "./images/cavalo.jpeg" },
-	{ name: "vaca", imageLink: "./images/la_vaca_saturno.jpeg" },
-	{ name: "tripi", imageLink: "./images/trulimero.jpeg" },
-	{ name: "trulimero", imageLink: "./images/trulimero.jpeg" },
-	{ name: "brasilini", imageLink: "./images/brasilini.png" },
+  { name: "bombardiro", imageLink: "./images/bombardiro_crocodilo.png" },
+  { name: "tralalero", imageLink: "./images/tralaleiro.jpeg" },
+  { name: "lirili", imageLink: "./images/lirili_larila.jpeg" },
+  { name: "sahur", imageLink: "./images/tung_tunh.jpeg" },
+  { name: "cavallo", imageLink: "./images/cavalo.jpeg" },
+  { name: "vaca", imageLink: "./images/la_vaca_saturno.jpeg" },
+  { name: "tripi", imageLink: "./images/trulimero.jpeg" },
+  { name: "trulimero", imageLink: "./images/trulimero.jpeg" },
+  { name: "brasilini", imageLink: "./images/brasilini.png" },
 ];
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
 
-	if ("data" in command && "execute" in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(
-			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-		);
-	}
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
 }
 
+console.log(
+  `Commands registered with the client: ${Array.from(client.commands.keys()).join(
+    ", "
+  )}`
+);
+
 client.once("ready", () => {
-	console.log(`Ready! Logged in as ${client.user.tag}`);
+  console.log(`Ready! Logged in as ${client.user.tag}`);
 });
 
 client.on("interactionCreate", async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({
-			content: "There was an error while executing this command!",
-			ephemeral: true,
-		});
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
--- brainrot/deploy-commands.js
+++ brainrot/deploy-commands.js
@@ -9,39 +9,36 @@ const guildId = process.env.GUILD_ID;
 
 const commands = [];
 const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  if ("data" in command && "execute" in command) {
-    commands.push(command.data.toJSON());
-  } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-    );
-  }
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ("data" in command && "execute" in command) {
+		commands.push(command.data.toJSON());
+	} else {
+		console.log(
+			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+		);
+	}
 }
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
-  try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
-    );
+	try {
+		console.log(
+			`Started refreshing ${commands.length} application (/) commands for guild ${guildId}.`
+		);
 
-    const data = await rest.put(
-      Routes.applicationCommands(clientId), 
-      { body: commands }
-    );
+		const data = await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+			body: commands,
+		});
 
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
-  } catch (error) {
-    console.error(error);
-  }
+		console.log(
+			`Successfully reloaded ${data.length} application (/) commands for guild ${guildId}.`
+		);
+	} catch (error) {
+		console.error(error);
+	}
 })();
